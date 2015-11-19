a48ba57ceee89d8ac60ab55511eb5eaf9576550f
aaaaatoz <rafa.xu.au@gmail.com>
2015-11-19 23:57:17 +1100

    reference

---
+++ referene.js
@@ -0,0 +1,14 @@
+/**
+ * Created by rafaxu on 19/11/2015.
+ */
+var Rafa = {
+    "favfood":"prawn",
+    "age": 30
+}
+
+var Person = Rafa ;
+
+Person.favfood = "salad";
+
+
+console.log(Rafa.favfood)
