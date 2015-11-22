f1615b3d058633393639961acfb621a8ff41fb7b
aaaaatoz <rafa.xu.au@gmail.com>
2015-11-22 20:35:33 +1100

    add module

---
+++ modules.js
@@ -0,0 +1,7 @@
+var movie = require("./movies.js");
+
+movie.avatar;
+
+movie.printHaha();
+
+console.log(movie.favoriteMovie);
---
+++ movies.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    printHaha: function (){
+        console.log("my haha");
+    },
+
+    favoriteMovie: "Matrix"
+}
+
+function printChappie(){
+    console.log("Avatar: PG-13");
+}
+
+function printAvatar(){
+    console.log("Avatar: PG-13");
+}
+
+module.exports.avatar = printAvatar()
