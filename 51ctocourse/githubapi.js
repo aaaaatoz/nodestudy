1de7d9534d43bcf63ae664678fd02659752f4877
aaaaatoz <rafa.xu.au@gmail.com>
2016-01-03 17:50:02 +1100

    add http request

---
+++ 51ctocourse/githubapi.js
@@ -0,0 +1,39 @@
+var https = require("https");
+
+var username = "aaaaatoz";
+
+var getrepo = function (username, callback) {
+    var options = {
+        host: 'api.github.com',
+        path: '/users/' + username + '/repos',
+        method: 'GET',
+        headers: {'user-agent': 'Mozilla/5.0'},
+    };
+
+    var request = https.request(options, function (response) {
+        var body = '';
+        response.on('data', function (chunk) {
+            body += chunk.toString('utf8');
+        });
+        response.on('end', function () {
+            var json = JSON.parse(body);
+            var repos = [];
+            json.forEach(function (repo) {
+                repos.push({
+                    name: repo.name,
+                    description: repo.description
+                })
+            })
+            callback(repos);
+        });
+    });
+    request.end();
+};
+
+getrepo("aaaaatoz", function (repo) {
+    console.log("aaaatoz has " + repo.length + " repos");
+})
+
+
+
+
